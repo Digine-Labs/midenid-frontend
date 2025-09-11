ff2edbbb0c6d9b965f9bf2362bb2b267903abb79
ermvrs <ermvrs@live.com>
2025-09-11 12:19:11 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Miden ID</h1>
+      <div className="card">
+
+        <p>
+          Miden Identity service
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Digine Labs 2025
+      </p>
+    </>
+  )
+}
+
+export default App
